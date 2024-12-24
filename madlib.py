deaf23525dd04834135328902ce2020b7cdc69ff
NipunHimeshaFernando <nipunhimesha@gmail.com>
2024-12-24 06:48:12 +0530

    created file1

---
+++ madlib.py
@@ -0,0 +1,13 @@
+#print("Hi all")
+
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"\nComputer programming is so {adj}! it makes me so excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+#madlib = f"\nComputer programming is so {adj}! it makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
